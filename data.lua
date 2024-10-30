718cc407300c17cc1d773fa9b353b602f9f9147b
jsu <hnz.such@seznam.cz>
2024-10-30 14:53:17 +0100

    some aura data

--- data.lua
+++ data.lua
@@ -1,17 +1,35 @@
 IU = IU or {}
 
-IU.priestAuras = {
-
-}
-
-IU.paladinAuras = {
-
-}
-
-IU.evokerAuras = {
-
-}
-
-IU.monkAuras = {
-    
+IU.data = {
+    ["Death Knight"] = {},
+    ["Demon Hunter"] = {},
+    ["Druid"] = {
+        {name = "Mark of the Wild", id = 1126, default = true},
+        {name = "Thorns", id = 467, default = false},
+        {name = "Barkskin", id = 22812, default = false}
+    },
+    ["Evoker"] = {},
+    ["Hunter"] = {},
+    ["Mage"] = {},
+    ["Monk"] = {
+        {name = "Thuder Focus Tea", id = 116680, default = false},
+        {name = "Ancient Teachings", id = 388025, default = false}
+    },
+    ["Paladin"] = {
+        {name = "Divine Shield", id = 642, default = false},
+        {name = "Blessing of protection", id = 1022, default = false}
+    },
+    ["Priest"] = {
+        {name = "Desperate prayer", id = 19236, default = false},
+        {name = "Power Word: Fortitude", id = 21562, default = true},
+        {name = "Rapture", id = 47536, default = false},
+        {name = "Pain Suppression", id = 33206, default = false},
+        {name = "Inner Light", id = 355897, default = false},
+        {name = "Inner Shadow", id = 355898, default = false},
+        {name = "Power of the dark side", id = 198069, default = false}
+    },
+    ["Rogue"] = {},
+    ["Shaman"] = {},
+    ["Warlock"] = {},
+    ["Warrior"] = {}
 }
